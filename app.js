4d8dc76c63feaa9f98901444bf267e28cad0f08d
Adrian Tucci <adriangiulianotucci@gmail.com>
2021-08-08 17:53:29 -0300

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const morgan = require("morgan");
+
+app.use(morgan("combined"));
 
 app.get("/doble/:number", (req, res) => {
   res.send(`La respuesta es ${req.params.number * 2}!`);
@@ -8,6 +11,10 @@ app.get("/doble/:number", (req, res) => {
 
 app.get("/mitad/:number", (req, res) => {
   res.send(`La respuesta es ${req.params.number / 2}`);
+});
+
+app.get("/:nombre", (req, res) => {
+  res.send(`Hola ${req.params.nombre}!`);
 });
 
 app.listen(port, () => {
